dadff7a710ccfd3183c7275214a90fdebfbd828b
mddien <mddien@humaxdigital.com>
2018-11-06 14:01:46 +0700

    Custom Seekbar

--- app/src/main/java/com/example/mddien/test/MainActivity.java
+++ app/src/main/java/com/example/mddien/test/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mddien.test;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +23,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_example);
+        if (Build.VERSION.SDK_INT < 16)//before Jelly Bean Versions
+        {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else // Jelly Bean and up
+        {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int ui = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            if (decorView != null) {
+                decorView.setSystemUiVisibility(ui);
+            }
+            //Hide actionbar
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
+        setContentView(R.layout.activity_main);
+        //init();
+    }
 
+    private void init() {
         // simple text item with numbers 0 ~ 9
         ArrayList<String> itemTitles = new ArrayList<>();
-        for(int i = 0 ; i < 6 ; i ++){
+        for (int i = 0; i < 6; i++) {
             itemTitles.add(String.valueOf(i));
         }
 
@@ -103,9 +128,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ActivityFrequency.class));
             }
         });
+
+        Button btnTest = findViewById(R.id.btn_test);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
     }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT < 16)//before Jelly Bean Versions
+        {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        } else // Jelly Bean and up
+        {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int ui = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            if (decorView != null) {
+                decorView.setSystemUiVisibility(ui);
+            }
+            //Hide actionbar
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
+    }
 
     // you should extends CircularAdapter to add your custom item
     private class CircularItemAdapter extends CircularAdapter {
@@ -114,12 +170,12 @@ public class MainActivity extends AppCompatActivity {
         private LayoutInflater mInflater;
         private ArrayList<View> mItemViews;
 
-        public CircularItemAdapter(LayoutInflater inflater, ArrayList<String> items){
+        public CircularItemAdapter(LayoutInflater inflater, ArrayList<String> items) {
             this.mItemViews = new ArrayList<>();
             this.mItems = items;
             this.mInflater = inflater;
 
-            for(final String s : mItems){
+            for (final String s : mItems) {
                 View view = mInflater.inflate(R.layout.view_circular_item, null);
                 TextView itemView = (TextView) view.findViewById(R.id.bt_item);
                 itemView.setText(s);
@@ -144,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void removeItemAt(int i) {
-            if(mItemViews.size() > 0) {
+            if (mItemViews.size() > 0) {
                 mItemViews.remove(i);
                 notifyItemChange();
             }
